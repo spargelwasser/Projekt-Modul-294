28a70fcd5e7a9490e997c819471a7f0a5d450529
spargelwasser <julia@stocker.nu>
2025-01-27 20:46:13 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+let API_URL = "https://graphql.anilist.co";
+
+let query = `
+            query {
+                Page(page: 1, perPage: 20) {
+                    media(type: ANIME) {
+                        id
+                        title {
+                            romaji
+                        }
+                        coverImage {
+                            extraLarge
+                        }
+                        description
+                    }
+                }
+            }
+        `;
+
+function reload() {
+    fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query })
+    })
+    .then(response => response.json())
+    .then(data => {
+        let animeList = data.data.Page.media;
+        displayAnimeList(animeList);
+    })
+    .catch(error => {
+        console.error("Error fetching anime data:", error);
+    });
+}
+
+function displayAnimeList(animes) {
+    let container = document.querySelector("#animeList");
+    container.innerHTML = ""; // clear
+
+    animes.slice(0,10).forEach(anime => {
+        let card = document.createElement("div");
+        card.classList = "card"
+
+        let img = document.createElement("img");
+        img.src = anime.coverImage.extraLarge;
+        img.classList = "card-img-top";
+        img.alt = anime.title.romaji;
+
+        let cardBody = document.createElement("div");
+        cardBody.classList = "card-body";
+
+        let animeTitle = document.createElement("h5");
+        animeTitle.classList = "card-title";
+        animeTitle.innerHTML = anime.title.romaji;
+
+        let animeText = document.createElement("p");
+        animeText.classList = "card-text";
+        animeText = anime.description;
+
+        cardBody.append(animeTitle);
+        cardBody.append(animeText);
+        card.append(img);
+        card.append(cardBody);
+        container.append(card);
+    });
+}             
